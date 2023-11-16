b31047cf95ac15ba1a6e8615988583df822d9661
ricardodelimaaa <142522306+ricardodelimaaa@users.noreply.github.com>
2023-11-16 15:00:06 +0000

    Landing

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Footer = () => {
+return (
+    <div>
+<div className="card-footer bg-dark" style={{color:"white"}}>
+Ricardo jose de lima galindez edad 29 age
+</div>
+    </div>
+)
+}
+
+export default Footer   
